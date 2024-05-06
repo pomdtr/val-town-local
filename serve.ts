022d71e9cf2203607dc9ee078715b8e5faf1d7ff
pomdtr <achille.lacoin@gmail.com>
2024-05-06 17:29:41 +0200

    improve serve script

--- serve.ts
+++ serve.ts
@@ -1,6 +1,36 @@
 import { parseArgs } from "jsr:@std/cli/parse-args";
 import * as path from "jsr:@std/path";
 
+function getMainExport(
+  mod: any
+): { ok: true; value: any } | { ok: false; error: Error } {
+  if ("default" in mod) {
+    return { ok: true, value: mod.default };
+  }
+
+  // If the val has exactly one named export, we run that.
+
+  const exports = Object.keys(mod);
+
+  if (exports.length > 1) {
+    const error = new Error(
+      `Vals require a default export, or exactly one named export. This val exports: ${exports.join(
+        ", "
+      )}`
+    );
+    error.name = "ImportValError";
+    return { ok: false, error };
+  } else if (exports.length === 0) {
+    const error = new Error(
+      "Vals require a default export, or exactly one named export. This val has none."
+    );
+    error.name = "ImportValError";
+    return { ok: false, error };
+  }
+
+  return { ok: true, value: mod[exports[0]] };
+}
+
 const args = parseArgs(Deno.args, {
   string: ["port"],
 });
@@ -14,20 +44,12 @@ if (typeof filename !== "string") {
 
 const mod = await import(path.join(Deno.cwd(), filename));
 
-let handler;
-if (mod.default) {
-  handler = mod.default;
-} else {
-  const exports = Object.values(mod);
-  if (exports.length != 1) {
-    console.error(
-      "Vals require a default export, or exactly one named export."
-    );
-    Deno.exit(1);
-  }
-
-  handler = exports[0];
+const exp = getMainExport(mod);
+if (!exp.ok) {
+  console.error(exp.error);
+  Deno.exit(1);
 }
+const handler = exp.value;
 
 Deno.serve(
   {
